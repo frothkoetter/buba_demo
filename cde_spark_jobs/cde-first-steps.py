b0e7ec5f9745db39070f77e89fa6b977e45113ee
Maximilian Engelhardt <mengelhardt@cloudera.com>
2024-05-23 13:18:35 +0200

    sync test #1

--- cde_spark_jobs/cde-first-steps.py
+++ cde_spark_jobs/cde-first-steps.py
@@ -1,11 +1,2 @@
 from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("CDE FIRST STEPS").getOrCreate()
-
-S3_BUCKET = "s3a://go01-demo/data/landing/"
-
-sales_df_2021 = spark.read.csv(f"{S3_BUCKET}/2021/sales.csv", header=True, inferSchema=True)
-sales_df_2021.show()
-
-sales_df_2022 = spark.read.csv(f"{S3_BUCKET}/2022/sales.csv", header=True, inferSchema=True)
-sales_df_2022.show()
+print("Hello world!")
