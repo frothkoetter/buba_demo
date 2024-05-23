1d774f64c87bc561a09d5126a68eac8a1a67a1d1
Maximilian Engelhardt <mengelhardt@cloudera.com>
2024-05-23 10:55:38 +0200

    path fix

--- cde_spark_jobs/cde-first-steps.py
+++ cde_spark_jobs/cde-first-steps.py
@@ -4,8 +4,8 @@ spark = SparkSession.builder.appName("CDE FIRST STEPS").getOrCreate()
 
 S3_BUCKET = "s3a://go01-demo/data/landing/"
 
-sales_df_2021 = spark.read.csv(f"{S3_BUCKET}/data/2021/sales.csv", header=True, inferSchema=True)
+sales_df_2021 = spark.read.csv(f"{S3_BUCKET}/2021/sales.csv", header=True, inferSchema=True)
 sales_df_2021.show()
 
-sales_df_2022 = spark.read.csv(f"{S3_BUCKET}/data/2022/sales.csv", header=True, inferSchema=True)
+sales_df_2022 = spark.read.csv(f"{S3_BUCKET}/2022/sales.csv", header=True, inferSchema=True)
 sales_df_2022.show()
